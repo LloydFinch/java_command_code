28d7ddf4909949dd3a0b0ee7fd6409499bd36aeb
王文奇 <wangwenqi@orangelab.cn>
2018-11-21 10:03:57 +0800

    add some code

--- hello/Hello.java
+++ hello/Hello.java
@@ -1,7 +1,12 @@
 
+import java.util.Random;
 
 class Hello{
 	public static void main(String []args){
+
+		Random random = new Random();
+		int a = random.nextInt(100);
 		System.out.println("hello world!");
+		System.out.println("next random = " + a);
 	}
 }
